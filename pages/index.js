692a5c987dbdc7ff17d1f1dfb839de652d73f3bc
akmalays <akmalays@gmail.com>
2022-02-23 22:22:41 +0700

    add assets folder

--- pages/index.js
+++ pages/index.js
@@ -4,5 +4,17 @@ import styles from "../styles/Home.module.css";
 import Homepage from "../pages/component/homePage";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <Homepage />
+      <div class="flex space-x-2 justify-center">
+        <button
+          type="button"
+          class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+        >
+          Button
+        </button>
+      </div>
+    </div>
+  );
 }
